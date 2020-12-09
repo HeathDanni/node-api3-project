beb41ace316a01c4b741494e1ef8f5b919eb1fe6
Heather Beckman <heatherbeckman85@gmail.com>
2020-12-08 20:57:25 -0700

    post middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const posts = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
-});
+  posts.get()
+    .then((post) => {
+      if (!post) {
+        return res.status(404).json({
+          message: "this user has no posts"
+        })
+      }
+      return res.status(200).json(post)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({
+        message: "could not retrieve user posts"
+      })
+    })
+  }
+);
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  
 });
 
 router.delete('/:id', (req, res) => {
@@ -20,9 +36,28 @@ router.put('/:id', (req, res) => {
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
-}
+function validatePostId() {
+  return (req, res, next) => {
+    posts.getById(req.params.id)
+      .then((post) => {
+        if (user) {
+          req.post = post
+          next()
+        } else {
+          res.status(404).json({
+            message: "invalid post id",
+          })
+          next()
+        }
+      }) 
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+          message: "Error retrieving post"
+        })
+      })
+    }
+  }
 
 module.exports = router;
 
